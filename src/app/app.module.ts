c2675108e98dd4fca8f39ee0b40bcdbe4ec0eaca
NikaKalandadze7 <nika.kalandadze71@gmail.com>
2025-01-20 18:25:46 +0400

    header, footer, main
    Added and styled header and footer, also created the base for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomeComponent } from './pages/home/home.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'contact', component: ContactComponent },
+  // { path: 'products', component: ProductsComponent },
+  // { path: 'products/:id', component: ProductComponent },
+  // {
+  //   path: 'dashboard',
+  //   component: DashboardComponent,
+  //   canActivate: [authGuard],
+  // },
+  // { path: '**', component: NotFoundComponent },
+];
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
-  providers: [
-    provideAnimationsAsync()
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+    RouterModule.forRoot(appRoutes),
   ],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
